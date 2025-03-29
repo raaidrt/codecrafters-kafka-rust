e556ea39cd7c16d435f208d6ace11fc7a9bb3813
Raaid Tanveer <raaidraiyantanveer@gmail.com>
2025-03-28 22:19:13 -0700

    Send result

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #![allow(unused_imports)]
+use std::error::Error;
+use std::io::prelude::*;
 use std::net::TcpListener;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                let buf: [u8; 2] = [0, 7];
+                stream.write(&buf)?;
                 println!("accepted new connection");
             }
             Err(e) => {
@@ -16,4 +20,5 @@ fn main() {
             }
         }
     }
+    Ok(())
 }
